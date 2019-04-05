f2533052d53ea569fe11863f11e8e68b99876d78
MARIUSZ BROZDA <mariusz.brozda@wp.pl>
2019-04-05 22:07:49 +0000

    Add basic game functions to run.py

--- run.py
+++ run.py
@@ -17,6 +17,37 @@ def write_to_file(filename, data):
         file.writelines(data)
         
         
+
+# add user's answer to file with wrong answers
+def add_user_answers(username, answer, correct_answer):
+    write_to_file("data/user_answers.json", "{0} - {1}    ( Correct - {2} )\n".format(
+                                    username, answer, correct_answer))
+
+
+          
+#  Function to get/read answers from the file (last five)
+def get_user_answers():
+    wrong_answers = []
+    with open("data/user_answers.json", "r+") as wrong_answer:
+        wrong_answers = wrong_answer.readlines()
+    return wrong_answers[-5:]
+
+
+# Function to add online users and points to user's ranking board
+def add_online_users(username, user_points):
+    user_points = user_points
+    online_users = get_online_users()
+    with open('data/online_users.txt', 'a') as ranking:
+        if not (username, user_points) in online_users:
+            ranking.write('\n{} : {}'.format(str(username), str(user_points)))
+
+
+#  Function to get online user from the file
+def get_online_users():
+    online_users=[]
+    with open("data/online_users.txt") as online_users:
+        online_users = online_users.readlines()
+    return online_users[-5:]
         
         
 
